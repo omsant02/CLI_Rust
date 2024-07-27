55d2260e917c763b37cc7510a32385ccaf2928ea
omsant02 <omsantoshwar02@gmail.com>
2024-07-27 19:58:17 +0530

    read args, store and print

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::env; // to read the values of command line arguments
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
