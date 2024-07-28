3faf336137015e48430d2fe3f995c8ee77f1478a
omsant02 <omsantoshwar02@gmail.com>
2024-07-28 20:51:50 +0530

    error handling-panic

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args);
-    
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
@@ -23,6 +23,10 @@ struct Config {
 
 impl Config {
     fn new(args: &[String]) -> Config {
+         if args.len() < 3 {
+            panic!("not enough arguments");
+        }
+        
         let query = args[1].clone();
         let file_path = args[2].clone();
 
