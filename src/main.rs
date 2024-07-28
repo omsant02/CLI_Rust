df43768feac4f0c32c2ed0a9e8e1d44bfa7e5c23
omsant02 <omsantoshwar02@gmail.com>
2024-07-28 23:27:37 +0530

    error handling-Result<>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env; // to read the values of command line arguments
 use std::fs;  // to handle files
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -22,14 +26,14 @@ struct Config {
 
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-         if args.len() < 3 {
-            panic!("not enough arguments");
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
         }
-        
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
