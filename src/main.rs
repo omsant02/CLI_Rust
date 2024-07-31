f12416531ddaaef46d8f2022d59eb9c3dc1551c2
omsant02 <omsantoshwar02@gmail.com>
2024-07-31 13:57:40 +0530

    use iterator

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,9 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
 
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // passing ownership of the iterator returned from env::args to Config::build directly.
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
